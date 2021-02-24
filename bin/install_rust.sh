3bf16677da8ff7e88a95dbe719994292464396eb
Jonas Sandberg <jonas.sandb@gmail.com>
2021-02-24 17:47:21 +0100

    Install scripts

--- bin/install_rust.sh
+++ bin/install_rust.sh
@@ -3,7 +3,7 @@
 if [[ ! -d $HOME/.cargo ]]; then
     curl https://sh.rustup.rs -sSf | sh -s -- --profile minimal --no-modify-path -y
 
-    crates='bat fd-find ripgrep exa uq diffr git-delta'
+    crates='bat fd-find ripgrep exa uq diffr git-delta xsv'
 
     for crate in $crates; do
         cargo install --locked --force $crate
