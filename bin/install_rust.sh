71b4ae8cda9fb1064443779c7780a39a9d5f0302
Jonas Sandberg <jonas.sandb@gmail.com>
2021-03-04 03:58:14 +0100

    Install script for zgen

--- bin/install_rust.sh
+++ bin/install_rust.sh
@@ -3,10 +3,11 @@
 if [[ ! -d $HOME/.cargo ]]; then
     curl https://sh.rustup.rs -sSf | sh -s -- --profile minimal --no-modify-path -y
 
-    crates='bat fd-find ripgrep exa uq diffr git-delta xsv'
+    crates='bat fd-find ripgrep exa uq git-delta xsv sd'
+    cargo install --locked --force $crates
 
-    for crate in $crates; do
-        cargo install --locked --force $crate
-    done
+    # for crate in $crates; do
+    #     cargo install --locked --force $crate
+    # done
 fi
 
---
+++ bin/install_zgen.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+if [[ ! -d $HOME/.zgen ]]; then
+    git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+fi
+
